c69dfbb481eecaa8a2fedb160bce1078c6d00545
monkeyMax-SVG <monkeymax@yeah.net>
2022-06-02 07:28:46 +0800

    added lptokens

---
+++ lpfinance-tokens/migrations/deploy.js
@@ -0,0 +1,96 @@
+// Migrations are an early feature. Currently, they're nothing more than this
+// single deploy script that's invoked from the CLI, injecting a provider
+// configured from the workspace's Anchor.toml.
+
+const anchor = require("@project-serum/anchor");
+const { ASSOCIATED_TOKEN_PROGRAM_ID, TOKEN_PROGRAM_ID, getAssociatedTokenAddress } = require('@solana/spl-token')
+const { PublicKey, SystemProgram, SYSVAR_RENT_PUBKEY, Keypair } = anchor.web3;
+
+const idl = require("../target/idl/lpfinance_tokens.json");
+const programID = idl.metadata.address;
+
+console.log("ProgramID", programID);
+
+const PREFIX = "lptokens";
+
+const lpsol_mint = "lpsol_mint";
+const lpusd_mint = "lpusd_mint";
+const lpdao_mint = "lpdao_mint";
+
+module.exports = async function (provider) {
+  // Configure client to use the provider.
+  anchor.setProvider(provider);
+
+  // Add your deploy script here
+  const program = new anchor.Program(idl, programID);
+
+  try {
+    // Find PDA from `cbs protocol` for state account
+    const [stateAccount, stateAccountBump] = await PublicKey.findProgramAddress(
+      [Buffer.from(PREFIX)],
+      program.programId
+    );
+    console.log("State-Account:", stateAccount.toBase58());
+
+    const [lpsolMint, lpsolMintBump] = await PublicKey.findProgramAddress(
+      [Buffer.from(PREFIX), Buffer.from(lpsol_mint)],
+      program.programId
+    );
+    console.log("LpSOL mint:", lpsolMint.toBase58());
+
+    const [lpusdMint, lpusdMintBump] = await PublicKey.findProgramAddress(
+      [Buffer.from(PREFIX), Buffer.from(lpusd_mint)],
+      program.programId
+    );
+    console.log("LpUSD Mint:", lpusdMint.toBase58());
+
+    const [lpdaoMint, lpdaoMintBump] = await PublicKey.findProgramAddress(
+      [Buffer.from(PREFIX), Buffer.from(lpdao_mint)],
+      program.programId
+    );
+    console.log("LpFI Mint:", lpdaoMint.toBase58());
+    
+    // Signer
+    const authority = provider.wallet.publicKey;
+
+    const configAccount = anchor.web3.Keypair.generate();
+    console.log("Config: ", configAccount.publicKey.toBase58());
+    // mint: PublicKey, owner: PublicKey, allowOwnerOffCurve?: boolean, programId?: PublicKey, associatedTokenProgramId?: PublicKey
+    const userDaotoken = await getAssociatedTokenAddress(
+      lpdaoMint, 
+      authority, 
+      true,
+      TOKEN_PROGRAM_ID,
+      ASSOCIATED_TOKEN_PROGRAM_ID
+    )
+
+    await program.rpc.initialize({
+      accounts: {
+        authority,
+        stateAccount,
+        config: configAccount.publicKey,
+        lpsolMint,
+        lpusdMint,
+        lpdaoMint,
+        userDaotoken,
+        systemProgram: SystemProgram.programId,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+      },
+      signers: [configAccount]
+    });
+
+  } catch (err) {
+    console.log("Transaction error: ", err);
+  }
+}
+
+// 2022-06-01
+// ProgramID GaVnsa8z34xSeYNDydTAdbiT64KxMgVXBXnimXXJT4Hw
+// State-Account: 64iaARaRU9sXwLmAVy1a5NkYVM82GJ9Lvk2VfJ8PMChk
+// Config:  3Lpjwy6tGj4XQVJBMcr8ESRpLDgdat3ozedQD5AjSf5a
+
+// LpSOL mint: 5jmsfTrYxWSKgrZp4Y8cziTWvt7rqmTCiJ75FbLqFTVZ
+// LpUSD Mint: 3GB97goPSqywzcXybmVurYW7jSxRdGuS28nj74W8fAtL
+// LpFI Mint: 3x96fk94Pp4Jn2PWUexAXYN4eLK8TVYXHUippdYCHK1p
---
+++ lpfinance-tokens/programs/lpfinance-tokens/src/lib.rs
@@ -0,0 +1,515 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{self, Mint, MintTo, Burn, Token, TokenAccount }
+};
+
+declare_id!("GaVnsa8z34xSeYNDydTAdbiT64KxMgVXBXnimXXJT4Hw");
+
+const LP_TOKEN_DECIMALS: u8 = 9;
+const PREFIX: &str = "lptokens";
+
+// DAO governance token
+const INITIAL_SUPPLY: u64 = 500000000; // 500,000,000
+
+const DAY_IN_SECONDS: i64 = 86400; 
+// Reward Rate => 0.00809%
+// so need to divide with 10000
+const DAILY_REWARD_RATE: u64 = 10000809;
+const DENOMINATOR: u64 =       10000000;
+
+#[program]
+pub mod lpfinance_tokens {
+    use super::*;
+    pub fn initialize(
+        ctx: Context<Initialize>
+    ) -> Result<()> {
+        msg!("INITIALIZE TOKEN PROGRAM");
+
+        let state_account = &mut ctx.accounts.state_account;
+        let config = &mut ctx.accounts.config;
+
+        state_account.owner = ctx.accounts.authority.key();
+
+        config.lpsol_mint = ctx.accounts.lpsol_mint.key();
+        config.lpusd_mint = ctx.accounts.lpusd_mint.key();
+        config.lpdao_mint = ctx.accounts.lpdao_mint.key();
+        config.state_account = ctx.accounts.state_account.key();
+        config.last_mint_timestamp = 0;
+
+        // INITIAL SUPPLY
+        let (mint_token_authority, mint_token_authority_bump) = 
+        Pubkey::find_program_address(&[PREFIX.as_bytes()], ctx.program_id);
+    
+        if mint_token_authority != ctx.accounts.state_account.key() {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+
+        // Mint
+        let seeds = &[
+            PREFIX.as_bytes(),
+            &[mint_token_authority_bump]
+        ];
+        let signer = &[&seeds[..]];
+
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.lpdao_mint.to_account_info(),
+            to: ctx.accounts.user_daotoken.to_account_info(),
+            authority: ctx.accounts.state_account.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+
+        token::mint_to(cpi_ctx, INITIAL_SUPPLY * 1000000000)?;
+
+        Ok(())
+    }
+    // Let CBS to mint tokens
+    pub fn mint_lptoken(
+        ctx: Context<MintLpToken>,
+        amount: u64
+    ) -> Result<()> {
+        if amount == 0 {
+            return Err(ErrorCode::InvalidAmount.into());
+        }
+        if ctx.accounts.config.cbs_account != ctx.accounts.signer.key() && ctx.accounts.config.staking_account != ctx.accounts.signer.key() {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+        let (mint_token_authority, mint_token_authority_bump) = 
+            Pubkey::find_program_address(&[PREFIX.as_bytes()], ctx.program_id);
+        
+        if mint_token_authority != ctx.accounts.state_account.key() {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+
+        // Mint
+        let seeds = &[
+            PREFIX.as_bytes(),
+            &[mint_token_authority_bump]
+        ];
+        let signer = &[&seeds[..]];
+
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.lptoken_mint.to_account_info(),
+            to: ctx.accounts.user_lptoken.to_account_info(),
+            authority: ctx.accounts.state_account.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+
+        token::mint_to(cpi_ctx, amount)?;
+        Ok(())
+    }
+
+    // Let CBS to burn tokens
+    pub fn burn_lptoken(
+        ctx: Context<BurnLpToken>,
+        amount: u64
+    ) -> Result<()> {
+        if amount == 0 {
+            return Err(ErrorCode::InvalidAmount.into());
+        }
+
+        let (mint_token_authority, mint_token_authority_bump) = 
+            Pubkey::find_program_address(&[PREFIX.as_bytes()], ctx.program_id);
+        
+        if mint_token_authority != ctx.accounts.state_account.key() {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+
+        // Mint
+        let seeds = &[
+            PREFIX.as_bytes(),
+            &[mint_token_authority_bump]
+        ];
+        let signer = &[&seeds[..]];
+
+        let cpi_accounts = Burn {
+            mint: ctx.accounts.lptoken_mint.to_account_info(),
+            from: ctx.accounts.cbs_lptoken.to_account_info(),
+            authority: ctx.accounts.state_account.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+
+        token::burn(cpi_ctx, amount)?;
+        Ok(())
+    }
+
+    // Owner can mint token
+    pub fn owner_mint_lptoken(
+        ctx: Context<OwnerLpToken>,
+        amount: u64
+    ) -> Result<()> {
+        if amount == 0 {
+            return Err(ErrorCode::InvalidAmount.into());
+        }
+
+        let (mint_token_authority, mint_token_authority_bump) = 
+        Pubkey::find_program_address(&[PREFIX.as_bytes()], ctx.program_id);
+    
+        if mint_token_authority != ctx.accounts.state_account.key() {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+
+        // Mint
+        let seeds = &[
+            PREFIX.as_bytes(),
+            &[mint_token_authority_bump]
+        ];
+        let signer = &[&seeds[..]];
+
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.lptoken_mint.to_account_info(),
+            to: ctx.accounts.user_lptoken.to_account_info(),
+            authority: ctx.accounts.state_account.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+
+        token::mint_to(cpi_ctx, amount)?;
+        Ok(())
+    }
+
+    pub fn owner_burn_lptoken(
+        ctx: Context<OwnerLpToken>,
+        amount: u64
+    ) -> Result<()> {
+        if amount == 0 {
+            return Err(ErrorCode::InvalidAmount.into());
+        }
+
+        // Burn
+        let cpi_accounts = Burn {
+            mint: ctx.accounts.lptoken_mint.to_account_info(),
+            from: ctx.accounts.user_lptoken.to_account_info(),
+            authority: ctx.accounts.owner.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        token::burn(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+
+    pub fn update_cbs_account(
+        ctx: Context<UpdateConfigAccount>,
+        new_cbs: Pubkey
+    ) -> Result<()> {
+        let config = &mut ctx.accounts.config;
+        
+        if ctx.accounts.state_account.owner != ctx.accounts.owner.key() {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+
+        config.cbs_account = new_cbs;
+        Ok(())
+    }
+
+    pub fn update_staking_account(
+        ctx: Context<UpdateConfigAccount>,
+        new_staking: Pubkey
+    ) -> Result<()> {
+        let config = &mut ctx.accounts.config;
+        
+        if ctx.accounts.state_account.owner != ctx.accounts.owner.key() {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+
+        config.staking_account = new_staking;
+        Ok(())
+    }
+
+    pub fn update_owner(
+        ctx: Context<UpdateConfigAccount>,
+        new_owner: Pubkey
+    ) -> Result<()> {
+        let state_account = &mut ctx.accounts.state_account;
+        if state_account.owner != ctx.accounts.owner.key() || ctx.accounts.owner.key() == new_owner {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+
+        state_account.owner = new_owner;
+
+        Ok(())
+    }
+
+    pub fn update_second_owner(
+        ctx: Context<UpdateConfigAccount>,
+        new_owner: Pubkey
+    ) -> Result<()> {
+        let state_account = &mut ctx.accounts.state_account;
+        if state_account.owner != ctx.accounts.owner.key() || state_account.second_owner == new_owner {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+
+        state_account.second_owner = new_owner;
+
+        Ok(())
+    }
+
+    // DAIL Mint DAO Token
+    pub fn mint_dao_lptoken (
+        ctx: Context<MintDaoLpToken>
+    ) -> Result<()> {
+        let total_supply = ctx.accounts.lptoken_mint.supply;
+        let config = &mut ctx.accounts.config;
+
+        if total_supply == 0 {
+            return Err(ErrorCode::InvalidAmount.into());
+        }
+
+        if ctx.accounts.state_account.second_owner != ctx.accounts.owner.key() {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+
+        let clock = Clock::get()?; // Returns real-world time in second uint
+        let dur_seconds = clock.unix_timestamp  - config.last_mint_timestamp ;
+        if dur_seconds < DAY_IN_SECONDS {
+            return Err(ErrorCode::TooOftenMint.into());
+        }
+        config.last_mint_timestamp = clock.unix_timestamp;
+
+        let mint_amount = total_supply * (DAILY_REWARD_RATE - DENOMINATOR) / DENOMINATOR;
+
+
+        let (mint_token_authority, mint_token_authority_bump) = 
+        Pubkey::find_program_address(&[PREFIX.as_bytes()], ctx.program_id);
+    
+        if mint_token_authority != ctx.accounts.state_account.key() {
+            return Err(ErrorCode::InvalidOwner.into());
+        }
+
+        // Mint
+        let seeds = &[
+            PREFIX.as_bytes(),
+            &[mint_token_authority_bump]
+        ];
+        let signer = &[&seeds[..]];
+
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.lptoken_mint.to_account_info(),
+            to: ctx.accounts.user_lptoken.to_account_info(),
+            authority: ctx.accounts.owner.to_account_info(),
+        };
+
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer);
+
+        token::mint_to(cpi_ctx, mint_amount)?;
+        Ok(())
+    }
+}
+
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    // Token program owner
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    // State Accounts
+    #[account(init,
+        seeds = [PREFIX.as_bytes()],
+        bump,
+        space = TokenStateAccount::LEN + 8,
+        payer = authority
+    )]
+    pub state_account: Box<Account<'info, TokenStateAccount>>,
+
+    // Config Accounts
+    #[account(init,
+        space = Config::LEN + 8,
+        payer = authority
+    )]
+    pub config: Box<Account<'info, Config>>,
+
+    #[account(init,
+        mint::decimals = LP_TOKEN_DECIMALS,
+        mint::authority = state_account,
+        seeds = [PREFIX.as_bytes(), b"lpsol_mint".as_ref()],
+        bump,
+        payer = authority
+    )]
+    pub lpsol_mint: Box<Account<'info, Mint>>,  
+
+    #[account(init,
+        mint::decimals = LP_TOKEN_DECIMALS,
+        mint::authority = state_account,
+        seeds = [PREFIX.as_bytes(), b"lpusd_mint".as_ref()],
+        bump,
+        payer = authority
+    )]
+    pub lpusd_mint: Box<Account<'info, Mint>>,
+
+    // This is LPFI token (DAO)
+    #[account(init,
+        mint::decimals = LP_TOKEN_DECIMALS,
+        mint::authority = state_account,
+        seeds = [PREFIX.as_bytes(), b"lpdao_mint".as_ref()],
+        bump,
+        payer = authority
+    )]
+    pub lpdao_mint: Box<Account<'info, Mint>>,
+
+    #[account(
+        init_if_needed,
+        payer = authority,
+        associated_token::mint = lpdao_mint,
+        associated_token::authority = authority
+    )]
+    pub user_daotoken: Box<Account<'info, TokenAccount>>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>
+}
+
+#[derive(Accounts)]
+pub struct MintLpToken<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut)]
+    pub state_account: Box<Account<'info, TokenStateAccount>>,
+    #[account(mut)]
+    pub config: Box<Account<'info, Config>>,
+    #[account(mut,
+        constraint = user_lptoken.mint == lptoken_mint.key(),
+    )]
+    pub user_lptoken: Box<Account<'info, TokenAccount>>,
+    #[account(mut)]
+    pub lptoken_mint: Account<'info, Mint>,
+    // Programs and Sysvars
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>
+}
+
+#[derive(Accounts)]
+pub struct BurnLpToken<'info> {
+    #[account(mut)]
+    pub cbs_account: Signer<'info>,
+    #[account(mut)]
+    pub state_account: Box<Account<'info, TokenStateAccount>>,
+    #[account(mut, has_one = cbs_account)]
+    pub config: Box<Account<'info, Config>>,
+    #[account(
+        mut,
+        constraint = cbs_lptoken.mint == lptoken_mint.key(),
+        constraint = cbs_lptoken.owner == cbs_account.key()
+    )]
+    pub cbs_lptoken: Box<Account<'info, TokenAccount>>,
+    #[account(mut)]
+    pub lptoken_mint: Account<'info, Mint>,
+    // Programs and Sysvars
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub rent: Sysvar<'info, Rent>
+}
+
+#[derive(Accounts)]
+pub struct OwnerLpToken<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(mut)]
+    pub state_account: Box<Account<'info, TokenStateAccount>>,
+    #[account(
+        init_if_needed,
+        payer = owner,
+        associated_token::mint = lptoken_mint,
+        associated_token::authority = owner
+    )]
+    pub user_lptoken: Box<Account<'info, TokenAccount>>,
+    #[account(mut)]
+    pub lptoken_mint: Account<'info, Mint>,
+    // Programs and Sysvars
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>
+}
+
+#[derive(Accounts)]
+pub struct MintDaoLpToken<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(mut, has_one = owner)]
+    pub state_account: Box<Account<'info, TokenStateAccount>>,
+    #[account(
+        init_if_needed,
+        payer = owner,
+        associated_token::mint = lptoken_mint,
+        associated_token::authority = owner
+    )]
+    pub user_lptoken: Box<Account<'info, TokenAccount>>,
+    #[account(mut, has_one = state_account)]
+    pub config: Box<Account<'info, Config>>,
+    #[account(mut)]
+    pub lptoken_mint: Account<'info, Mint>,
+    // Programs and Sysvars
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>
+}
+
+#[derive(Accounts)]
+pub struct UpdateConfigAccount<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(mut, has_one = owner)]
+    pub state_account: Box<Account<'info, TokenStateAccount>>,
+    #[account(mut, has_one = state_account)]
+    pub config: Box<Account<'info, Config>>
+}
+
+#[account]
+#[derive(Default)]
+pub struct TokenStateAccount {
+    pub owner: Pubkey,
+    pub second_owner: Pubkey
+}
+
+impl TokenStateAccount {
+    pub const LEN: usize = 2 * 32;
+}
+
+#[account]
+#[derive(Default)]
+pub struct Config {
+    pub state_account: Pubkey,
+    pub cbs_account: Pubkey,
+    pub staking_account: Pubkey,
+    pub lpusd_mint: Pubkey,
+    pub lpsol_mint: Pubkey,
+    pub lpdao_mint: Pubkey,
+    pub last_mint_timestamp: i64 // 8
+}
+
+impl Config {
+    pub const LEN: usize = 6 * 32 + 8;
+}
+
+#[derive(AnchorDeserialize, AnchorSerialize, Default, Clone)]
+pub struct ProgramBumps {
+    pub state_account: u8,
+    pub lpsol_mint: u8,
+    pub lpusd_mint: u8,
+    pub lpdao_mint: u8
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Invalid Amount")]
+    InvalidAmount,
+    #[msg("Invalid Owner")]
+    InvalidOwner,
+    #[msg("Too often mint")]
+    TooOftenMint
+}
