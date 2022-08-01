3a1b46f5bf32e145c07d9a2e885a5b983cf3dee2
monkeyMax-SVG <monkeymax@yeah.net>
2022-08-01 13:28:57 +0800

    added table data

---
+++ lpfinance-swap/ts/1_manage_pool/4_get_table_data.ts
@@ -0,0 +1,41 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { LpfinanceSwap } from "../../target/types/lpfinance_swap";
+import { SignerWallet } from "@saberhq/solana-contrib";
+import {
+  Connection,
+} from "@solana/web3.js";
+import {
+  getCreatorKeypair, getPublicKey,
+} from "../utils";
+import { NETWORK } from "../config";
+
+const get_table_data = async () => {
+    
+  const creatorKeypair = getCreatorKeypair();
+
+  const connection = new Connection(NETWORK, "confirmed");
+
+  const provider = new SignerWallet(creatorKeypair).createProvider(connection);
+  // console.log(provider)
+  anchor.setProvider(new anchor.AnchorProvider(connection, provider.wallet, anchor.AnchorProvider.defaultOptions()));
+  const program = anchor.workspace.LpfinanceSwap as Program<LpfinanceSwap>;
+  
+  try {
+    const pool_pubkey = await getPublicKey("lpfi-usdc-pool");
+    const poolAccount = await program.account.poolInfo.fetch(pool_pubkey);
+
+    const total_lp_amount = Number(poolAccount.totalLpAmount);
+    const feeRate = Number(poolAccount.fee) / 1000;
+    const lpTokenPrice = 2;
+    const Liquidity =  lpTokenPrice * total_lp_amount;
+    console.log("Liquidity: ", Liquidity);
+  } catch(e) {
+    console.log("failed");
+    console.log(e);
+  }
+
+
+};
+
+get_table_data();
